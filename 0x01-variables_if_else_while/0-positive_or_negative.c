de644082b99d310ae97c6861db9b0104ffc28fc9
abdulbasit <abdulbasitkiggundu@gmail.com>
2022-10-15 14:42:23 -0700

    Assigning a number to a variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - prints if the number is postive,negative or 0
  * Return:Always 0
@@ -19,6 +20,7 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
+	}
 	else
 	{
 		printf("%d is negative\n", n);
