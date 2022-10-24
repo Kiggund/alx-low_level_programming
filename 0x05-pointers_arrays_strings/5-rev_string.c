a994fe4a416da30ad69e6eb4e793ee9968510505
abdulbasit <abdulbasitkiggundu@gmail.com>
2022-10-24 14:10:32 -0700

    update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,15 @@ void rev_string(char *s)
 {
 	char rv = s[0];
 	int c = 0;
+	int i;
 
-	while (s[c] != '\0'; i++)
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
 	{
-		if ((i % 2) == 0)
-			_putchar (str[i]);
-		else
-			continue;
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
 	}
-	_putchar ('\n);
 }
