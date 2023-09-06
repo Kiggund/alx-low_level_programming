8a183989c7fe51e780f9e6a2de0d7b196ac04562
kiggund <abdulbasitkiggundu@gmail.com>
2023-09-06 16:31:43 -0700

    my tasks

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void_puts(char*str)
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
