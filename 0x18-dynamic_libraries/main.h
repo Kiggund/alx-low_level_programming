76ecbcb6517300979d1406ef407394abd12fc94c
kiggund <abdulbasitkiggund@gmail.com>
2023-11-17 01:11:41 -0800

    my files

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+int _isdigit(int c);
+int _strlen(char *s);
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+
+#endif
